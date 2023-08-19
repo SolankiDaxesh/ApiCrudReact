3c06695b80e272629ce84220fd3c507916106fca
SolankiDaxesh <solankidaxesh912@gmail.com>
2023-08-19 16:09:27 +0530

    1 file modified

--- src/components/Read.js
+++ src/components/Read.js
@@ -1,33 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import { Table } from 'semantic-ui-react';
 export default function Read() {
-  const [APIData, setAPIData] = useState([]);
+  const [data, setData] = useState([]);
   useEffect(() => {
-    fetch('https://dummyjson.com/products/category/smartphones')
-      .then((res) => res.json())
-      .then(console.log);
+    fetch('https://dummyjson.com/products/')
+      .then((response) => response.json())
+      .then((data) => setData(data));
   }, []);
   return (
     <div>
-      <Table singleLine>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Name</Table.HeaderCell>
-            <Table.HeaderCell>Registration Date</Table.HeaderCell>
-            <Table.HeaderCell>E-mail address</Table.HeaderCell>
-            <Table.HeaderCell>Premium Plan</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
+      <p></p>
+      <table border="1" color="white">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* {data.forEach((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.title}</td>
+              <td>{item.description}</td>
+            </tr>
+          ))} */}
+          {/* {data.map((products, index) => {
+            return (
+              <tr key={products.index}>
+                <td>{products.id}</td>
+                <td>{products.title}</td>
+                <td>{products.description}</td>
+              </tr>
+            );
+          })}
+           */}
 
-        <Table.Body>
-          <Table.Row>
-            <Table.Cell>John Lilki</Table.Cell>
-            <Table.Cell>September 14, 2013</Table.Cell>
-            <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
-            <Table.Cell>No</Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
+          {data.map((products, index) => {
+            return (
+              <tr key={index}>
+                <td>{products.id}</td>
+                <td>{products.title}</td>
+                <td>{products.description}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
